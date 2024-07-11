2200debdb37d55b908c648e237550df6ce760ffd
Prawesh kumar <143396429+Praweshkumar@users.noreply.github.com>
2024-07-11 11:22:31 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,25 @@
+const display = document.getElementById("inputBox");
+const buttons = document.querySelectorAll("button");
+
+let buttonsArray = Array.from(buttons);
+
+let string = "";
+
+buttonsArray.forEach((btn) => {
+  // console.log(buttonsArray);
+  btn.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      display.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      display.value = string;
+    } else if (e.target.innerHTML == "=") {
+      string = eval(string);
+      display.value = string;
+    } else {
+      string += e.target.innerHTML;
+      display.value = string;
+    }
+  });
+});
